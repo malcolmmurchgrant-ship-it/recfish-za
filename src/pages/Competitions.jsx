b2863c6353b9aa39217be56341b0a49db8c557cf
Malcolm Grant <malcolmmurchgrant@gmail.com>
2026-07-01 12:21:04 +0200

    Update Competitions.jsx
    Junior Gamefish Nationals Cancelled

--- src/pages/Competitions.jsx
+++ src/pages/Competitions.jsx
@@ -94,7 +94,7 @@ function getCompConfig(comp, canEnter) {
     'ad8d03e7-6d4d-4cea-b96b-1007d3c6127d': { hideGenericAdmin: true, links: [{ to: null, label: '📊 Results Coming Soon', primary: true, disabled: true }] },  // All Inland Interprov 2025
     '350c759c-fa02-48b1-9c77-bf3b40fff22f': { hideGenericAdmin: true, links: [{ to: null, label: '📊 Results Coming Soon', primary: true, disabled: true }] },  // Gamefish Nationals 2025
     'b5d55428-53fc-4ce6-ad76-f098f757701a': { hideGenericAdmin: true, links: [{ to: null, label: '📊 Results Coming Soon', primary: true, disabled: true }] },  // All Coastals 2025
-    '0a099402-0bc5-46b0-b94d-0f94129758f0': { hideGenericAdmin: true, links: [{ to: null, label: '📊 Results Coming Soon', primary: true, disabled: true }] },  // Junior U16 Gamefish 2025
+    '0a099402-0bc5-46b0-b94d-0f94129758f0': { hideGenericAdmin: true, links: [{ to: null, label: '❌ Cancelled: Insufficient Participation', primary: true, disabled: true }] },  // Junior U16 Gamefish 2025
     '82dbbca2-fbea-4eb7-931c-a60072a136c1': { hideGenericAdmin: true, links: [{ to: null, label: '📊 Results Coming Soon', primary: true, disabled: true }] },  // Junior U19 Gamefish 2025
     'c9fce393-71f7-4f9e-85cf-163911ccb877': { hideGenericAdmin: true, links: [{ to: null, label: '📊 Results Coming Soon', primary: true, disabled: true }] },  // HT Billfish Interprov 2025
     '1c6ebb69-e11c-46d4-8894-ec00848bde37': { hideGenericAdmin: true, links: [{ to: null, label: '📊 Results Coming Soon', primary: true, disabled: true }] },  // Tuna International 2025
